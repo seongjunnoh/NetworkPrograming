2f6b0c2c2c4eb182c5d77b63cc5d83d70f83ebbf
seongjunnoh <seongjunnoh>
2024-04-03 13:32:59 +0900

    feat 스프링  프로젝트 생성

---
+++ src/main/java/org/example/icebraking/IceBrakingApplication.java
@@ -0,0 +1,13 @@
+package org.example.icebraking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceBrakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IceBrakingApplication.class, args);
+	}
+
+}
