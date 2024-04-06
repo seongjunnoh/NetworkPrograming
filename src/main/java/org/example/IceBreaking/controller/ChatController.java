711d799a74f8b65f1c1ca9dca799d7c00a685f42
seongjunnoh <seongjunnoh>
2024-04-06 21:04:02 +0900

    feat team별 id값 부여 & /team/all.html enter 버튼 추가

---
+++ src/main/java/org/example/IceBreaking/controller/ChatController.java
@@ -0,0 +1,10 @@
+package org.example.IceBreaking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+//    @GetMapping("/chat/{teamId}")
+}
--- src/main/java/org/example/IceBreaking/domain/Team.java
+++ src/main/java/org/example/IceBreaking/domain/Team.java
@@ -6,9 +6,13 @@ import lombok.Setter;
 @Getter @Setter
 public class Team {
 
+    private static int nextId = 1;
+
     private String teamName;
+    private int id;             // team의 고유 id 값
 
     public Team(String teamName) {
         this.teamName = teamName;
+        this.id = nextId++;
     }
 }
