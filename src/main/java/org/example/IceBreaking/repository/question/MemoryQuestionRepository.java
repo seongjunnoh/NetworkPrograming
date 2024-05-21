274aabe7428812e2b3319158a74a2780b7c75fcb
seongjunnoh <seongjunnoh>
2024-05-21 11:21:27 +0900

    feat team의 최대 빈도 관심사에 대한 질문들 중 랜덤으로 return, 추후 하위 우선순위 키워드에 대한 질문들도 뽑아주도록 리펙

--- src/main/java/org/example/IceBreaking/repository/question/MemoryQuestionRepository.java
+++ src/main/java/org/example/IceBreaking/repository/question/MemoryQuestionRepository.java
@@ -59,6 +59,7 @@ public class MemoryQuestionRepository implements QuestionRepository {
         teamInterestsMap.put(teamId, userInterestsMapInTeam);
 
         // 검증용
+        System.out.println("teamId = " + teamId);
         for (String key : userInterestsMapInTeam.keySet()) {
             System.out.println(key + " : " + Arrays.toString(userInterestsMapInTeam.get(key)));
         }
@@ -66,6 +67,37 @@ public class MemoryQuestionRepository implements QuestionRepository {
 
     @Override
     public Question findQuestionByTeamInterests(int teamId) {
-        return new Question("테스트용 예시질문");
+        // teamId별로 user들의 관심사 list 정보를 count & 가중치를 다르게 부여해서 질문 return
+        Map<String, String[]> userInterestsMapInTeam = teamInterestsMap.get(teamId);
+
+        // 관심사 빈도 수를 저장할 맵 -> user가 채팅방을 나갔다 들어올 경우, 관심사를 변경했을수도 있으므로 매번 새로 계산
+        // 추후에 리펙토링 필요할 듯
+        Map<String, Integer> interestCountMap = new HashMap<>();
+
+        for (String[] interests : userInterestsMapInTeam.values()) {
+            for (String interest : interests) {
+                interestCountMap.put(interest, interestCountMap.getOrDefault(interest, 0) + 1);
+            }
+        }
+
+        // 가장 빈도수가 높은 관심사를 select
+        int maxCount = Collections.max(interestCountMap.values());
+        List<String> topInterests = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : interestCountMap.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                topInterests.add(entry.getKey());
+            }
+        }
+
+        // topInterests에서 랜덤으로 하나 선택
+        Random random = new Random();
+        String topInterest = topInterests.get(random.nextInt(topInterests.size()));
+
+        // questionMap에서 topInterest에 해당하는 키워드의 질문들 get
+        QuestionsByInterestKey questionsByInterestKey = questionMap.get(InterestKeyword.valueOf(topInterest.toUpperCase()));
+        String[] questions = questionsByInterestKey.getQuestions();
+        String selectQuestion = questions[random.nextInt(questions.length)];
+
+        return new Question(selectQuestion);
     }
 }
