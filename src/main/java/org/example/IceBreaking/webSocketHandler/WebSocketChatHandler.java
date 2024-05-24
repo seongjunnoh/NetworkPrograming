824161d466bd96544d60c219599c6c991cdd5f1f
seongjunnoh <seongjunnoh>
2024-05-24 16:32:55 +0900

    feat 채팅방에서 exit버튼이 아니라 뒤로가기 버튼을 채팅방 나갈시에도 퇴장메시지 출력 기능 추가

--- src/main/java/org/example/IceBreaking/webSocketHandler/WebSocketChatHandler.java
+++ src/main/java/org/example/IceBreaking/webSocketHandler/WebSocketChatHandler.java
@@ -1,5 +1,6 @@
 package org.example.IceBreaking.webSocketHandler;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.IceBreaking.domain.Chat;
@@ -7,10 +8,15 @@ import org.example.IceBreaking.dto.ConnectDto;
 import org.example.IceBreaking.dto.UserInterestDto;
 import org.example.IceBreaking.repository.chat.ChatRepository;
 import org.example.IceBreaking.repository.question.QuestionRepository;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -24,12 +30,16 @@ public class WebSocketChatHandler extends TextWebSocketHandler {
     private final static String chatBot = "IceBreaker";
 
     @MessageMapping("/chat/enter")
-    public void enter(ConnectDto connectDto) {
+    public void enter(ConnectDto connectDto, SimpMessageHeaderAccessor headerAccessor) {
         connectDto.setMessage(connectDto.getUserName() + "님이 입장하였습니다.");
 
         // chat 저장
         Chat welcomeChat = new Chat(connectDto.getUserName(), connectDto.getMessage());
         chatRepository.saveChat(connectDto.getTeamId(), welcomeChat);
+
+        // SimpMessageHeaderAccessor에 teamId, userName 추가
+        headerAccessor.getSessionAttributes().put("teamId", connectDto.getTeamId());
+        headerAccessor.getSessionAttributes().put("userName", connectDto.getUserName());
 
         template.convertAndSend("/sub/chat/room/" + connectDto.getTeamId(), connectDto);
     }
@@ -76,4 +86,22 @@ public class WebSocketChatHandler extends TextWebSocketHandler {
         // userInterests 값을 받아서 팀별로 구분해 서버에 저장
         questionRepository.saveInterestsByTeam(userInterestDto);
     }
+
+    @EventListener
+    public void chatRoomExitListener(SessionDisconnectEvent event) {
+        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(event.getMessage());
+        int teamId = (int) headerAccessor.getSessionAttributes().get("teamId");
+        String userName = (String) headerAccessor.getSessionAttributes().get("userName");
+
+        // chat 저장
+        Chat exitChat = new Chat();
+        exitChat.setUserName(userName);
+        exitChat.setMessage(userName + "님이 나갔습니다.");
+        chatRepository.saveChat(teamId, exitChat);
+
+        // message send
+        ConnectDto connectDto = new ConnectDto(teamId, userName, exitChat.getMessage());
+        template.convertAndSend("/sub/chat/room/" + connectDto.getTeamId(), connectDto);
+    }
+
 }
