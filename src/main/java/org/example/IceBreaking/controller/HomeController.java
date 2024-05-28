ec0f3f8e1efe810bd724b33672fa3646ee89a0c6
seongjunnoh <seongjunnoh>
2024-05-28 11:01:17 +0900

    refactor homeController 불필요한 코드 제거

--- src/main/java/org/example/IceBreaking/controller/HomeController.java
+++ src/main/java/org/example/IceBreaking/controller/HomeController.java
@@ -1,17 +1,15 @@
 package org.example.IceBreaking.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(HttpServletRequest req, Model model) {
-        boolean loggedIn = req.getSession().getAttribute("loggedIn") != null;
-        model.addAttribute("loggedIn", loggedIn);
+    public String home() {
         return "home";
     }
 
