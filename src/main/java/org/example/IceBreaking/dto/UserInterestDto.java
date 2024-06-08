88155258e0740e4e5053be7ec722d969e4821f17
seongjunnoh <seongjunnoh>
2024-06-08 16:22:42 +0900

    refactor 사용하지않는 dto delete & 주석 추가

--- src/main/java/org/example/IceBreaking/dto/UserInterestDto.java
+++ src/main/java/org/example/IceBreaking/dto/UserInterestDto.java
@@ -8,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInterestDto {
+    // 해당 팀플방에 입장한 user의 관심사 정보를 서버로 전송하기 위한 dto
 
     private String userId;
     private int teamId;
